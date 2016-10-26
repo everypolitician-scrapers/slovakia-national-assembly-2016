db3e844248095307dce93c13e3a80dc024e62dee
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-26 12:47:10 +0100

    Add scraper
    Scrapes all member pages and saves data to mysql

---
+++ scraper.rb
@@ -0,0 +1,25 @@
+#!/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'pry'
+require 'require_all'
+require 'scraperwiki'
+require 'nokogiri'
+
+require_rel 'lib'
+
+require 'open-uri/cached'
+# require 'scraped_page_archive/open-uri'
+
+LIST_PAGE = 'http://www.nrsr.sk/web/?sid=poslanci/zoznam_abc'.freeze
+BASE = 'http://www.nrsr.sk/web/'.freeze
+
+members = AllMembersPage.new(LIST_PAGE)
+                        .to_h[:member_relative_urls]
+                        .map { |url| MemberPage.new(BASE + url).to_h }
+warn "Found #{members.count} members"
+
+members.each do |member|
+  ScraperWiki.save_sqlite([:id], member)
+end
